7ef3c59a570dabfeb10f7c13029fe2e59998750b
DouglasSimoesM <douglas.simoesm@gmail.com>
2024-12-04 11:08:51 -0300

    Exception

--- ContaBanco/src/ContaCorrente.java
+++ ContaBanco/src/ContaCorrente.java
@@ -1,32 +1,38 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ContaCorrente {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        try {
+            Scanner scan = new Scanner(System.in);
 
-        boolean validar = false;
-        int validaNumeroConta = 0;
-        String validaNomeCliente = "";
+            boolean validar = false;
+            int validaNumeroConta = 0;
+            String validaNomeCliente = "";
 
-        ContaTerminal conta = new ContaTerminal();
+            ContaTerminal conta = new ContaTerminal();
 
-        conta.inserirValores();
+            conta.inserirValores();
 
-        System.out.println("Consultar conta:");
-        while (!validar){
-            System.out.println("Entre com numero da Conta:");
-            validaNumeroConta = scan.nextInt();
+            System.out.println("Consultar conta:");
+            while (!validar) {
+                System.out.println("Entre com numero da Conta:");
+                validaNumeroConta = scan.nextInt();
 
-            System.out.println("Entre com Nome do cliente");
-            validaNomeCliente = scan.next();
+                System.out.println("Entre com Nome do cliente");
+                validaNomeCliente = scan.next();
 
-            if (validaNumeroConta == conta.numeroConta && validaNomeCliente.equalsIgnoreCase(conta.nomeCliente)){
-                conta.imprimirMsgm();
-                validar = true;
-            } else {
-                System.out.println("Numero da conta inexistente");
+                if (validaNumeroConta == conta.numeroConta && validaNomeCliente.equalsIgnoreCase(conta.nomeCliente)) {
+                    conta.imprimirMsgm();
+                    validar = true;
+                } else {
+                    System.out.println("Numero da conta inexistente");
+                }
             }
-        }
 
+        }catch (InputMismatchException e){
+            System.out.println("Os campos numero da conta e saldo devem ser preenchidos com valores numéricos" +
+                    " \nEx: conta: 123 saldo: 10,22");
+        }
     }
 }
